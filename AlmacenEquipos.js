a3a11a9607588acdc02443be425015da4e492e35
JavierClemente <javierclementejr@gmial.com>
2026-02-16 13:28:05 +0100

    Cambiamos las clases de Equipos.html y empezamos añadiendo 5 equipos de cada liga a AlmacenEquipos

--- AlmacenEquipos.js
+++ AlmacenEquipos.js
@@ -1,4 +1,41 @@
 const availableTeams = [
-    { id: 1, name: 'Real Madrid', liga: 'La Liga', titles: 104 },
-    { id: 2, name: 'Real Madrid', liga: 'La Liga', titles: 104 },
+    //La Liga
+    { id: 1, name: 'Real Madrid', ligue: 'La Liga', titles: 106 },
+    { id: 2, name: 'FC Barcelona', ligue: 'La Liga', titles: 103 },
+    { id: 3, name: 'Athletic Club', ligue: 'La Liga', titles: 36 },
+    { id: 4, name: 'Atletico de Madrid', ligue: 'La Liga', titles: 33 },
+    { id: 5, name: 'Valencia CF', ligue: 'La Liga', titles: 23 },
+
+    //Ligue 1
+    { id: 6, name: 'Paris Saint-Germain', ligue: 'Ligue 1', titles: 57 },
+    { id: 7, name: 'Olympique de Marsella', ligue: 'Ligue 1', titles: 28 },
+    { id: 8, name: 'A. S. Saint-Étienne', ligue: 'Ligue 1', titles: 24 },
+    { id: 9, name: 'Olympique de Lyon', ligue: 'Ligue 1', titles: 22 },
+    { id: 10, name: 'A. S. Monaco F.C.', ligue: 'Ligue 1', titles: 19 },
+
+    //Bundesliga
+    { id: 11, name: 'FC Bayern Múnich', ligue: 'Bundesliga', titles: 83 },
+    { id: 12, name: 'Borussia Dortmund', ligue: 'Bundesliga', titles: 23 },
+    { id: 13, name: 'FC Schalke 04', ligue: 'Bundesliga', titles: 17 },
+    { id: 14, name: 'Bayer Leverkusen', ligue: 'Bundesliga', titles: 4 },
+    { id: 15, name: 'RB Leipzig', ligue: 'Bundesliga', titles: 3 },
+
+    //Serie A
+    { id: 16, name: 'Juventus', ligue: 'Serie A', titles: 74 },
+    { id: 17, name: 'AC Milan', ligue: 'Serie A', titles: 53 },
+    { id: 18, name: 'Inter de Milán', ligue: 'Serie A', titles: 46 },
+    { id: 19, name: 'AS Roma', ligue: 'Serie A', titles: 16 },
+    { id: 20, name: 'Atalanta', ligue: 'Serie A', titles: 8 },
+
+    //Premier League
+    { id: 21, name: 'Manchester United', ligue: 'Premier League', titles: 68 },
+    { id: 22, name: 'Liverpool FC', ligue: 'Premier League', titles: 52 },
+    { id: 23, name: 'Arsenal FC', ligue: 'Premier League', titles: 48 },
+    { id: 24, name: 'Manchester City', ligue: 'Premier League', titles: 36 },
+    { id: 25, name: 'Chelsea', ligue: 'Premier League', titles: 36 },
+
+
+
+
+
 ]

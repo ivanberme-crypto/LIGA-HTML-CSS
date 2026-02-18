68eac507e21eb7d3965a1ce6dd5fffc8cdea9974
javierclementejr-sudo <javierclementejr@gmail.com>
2026-02-18 21:22:13 +0100

    Cambiamos el nombre de las clases del resto de las paginas que faltaban para que no haya errores en el css general al pasarlo

--- JavaScript/MenuDesplegable.js
+++ JavaScript/MenuDesplegable.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menuBtn = document.querySelector(".fd__header__menu-btn");
     const menuList = document.querySelector(".fd__header__list");
-    const desplegableBtn = document.querySelector(".fd__desplegable__btn");
-    const desplegableContent = document.querySelector(".fd__desplegable__content");
+    const desplegableBtn = document.querySelector(".fd__header__desplegable__btn");
+    const desplegableContent = document.querySelector(".fd__header__desplegable__content");
 
     menuBtn.addEventListener("click", () => {
         menuList.classList.toggle("show");
